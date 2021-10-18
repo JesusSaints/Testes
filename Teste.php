c6125482106935f7635aa3e1373cbe5d2fb6a689
JesusSaints <92698720+JesusSaints@users.noreply.github.com>
2021-10-18 19:02:42 -0300

    Teste
    OI tudo bem?

---
+++ Teste.php
@@ -0,0 +1,35 @@
+<?php
+	$paginas = ['Jesus'=>'Salvador','Home'=>'Senhor','Contato'=>'Criardor'];
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Jesus Cristo</title>
+	<style type="text/css">
+	*{
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		header{
+			background-color:#069;
+			padding:8 px 10px;
+			text-align: center;
+			
+
+		}
+	}
+		
+
+
+</head>
+<body>
+	<header>
+		
+	</header>
+</body>
+</html>
