77bae82b1ba19dcf4d44862594fdc060d2380e24
JesusSaints <92698720+JesusSaints@users.noreply.github.com>
2021-10-20 23:34:57 -0300

    Oi
    Ola

---
+++ Um.php
@@ -0,0 +1,46 @@
+<?php
+	$paginas = ["Jesus","Home","Contato"];
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Jesus Cristo</title>
+	<style type="text/css">
+		*{
+			margin: 0;
+			padding: 0;
+			box-sizing: border-box;
+
+		}
+		header{
+			background:#069;
+			padding: 8px 10px;
+			text-align: center;
+			
+
+
+		}
+
+
+
+	</style>
+</head>
+
+<body>
+	<header>
+		<?php foreach ($paginas as $key => $value) {
+			// code...
+		}
+
+
+
+
+
+		?>
+	</header>
+</body>
+</html>
