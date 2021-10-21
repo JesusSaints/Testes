642c085c44451f65decccb6dd28f94339eeae11f
JesusSaints <92698720+JesusSaints@users.noreply.github.com>
2021-10-20 23:26:18 -0300

    OI
    Ola

---
+++ Um.php
@@ -0,0 +1,36 @@
+<?php
+	$paginas = ["Jesus","Home","Contato"];
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Jesus Cristo</title>
+	<style type="text/css">
+		*{
+			margin: 0;
+			padding: 0;
+			box-sizing: border-box;
+
+		}
+		header{
+			background:#069;
+			padding: 8px 10px;
+			text-align: center;
+			
+
+
+		}
+
+
+
+	</style>
+</head>
+
+<body>
+
+</body>
+</html>
