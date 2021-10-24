25fbf60703684c2d4406c19ee6726cdcd4e9f9d8
JesusSaints <92698720+JesusSaints@users.noreply.github.com>
2021-10-24 17:32:10 -0300

    Ji.php

---
+++ Ji.php
@@ -0,0 +1,46 @@
+<?php
+	$paginas = ["Jesus"=>"Salvador","Home"=>"Eterno","Contato"=>"Senhor"];
+
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Jesus Cristo</title>
+	<style type="text/css">
+		*{
+			margin: 30px 0px;
+			padding: 0;
+			box-sizing: border-box;
+		}
+		header{
+			background: #006400;
+			padding: 15px 20px;
+			text-align: center;
+
+		}
+
+
+	</style>
+
+
+
+</head>
+<body>
+	<header>
+		<?php
+			foreach ($paginas as $key => $value) {
+				echo '<a href="?page='.$key.'">'.$key.'</a>';
+			}
+
+
+		?>
+	</header>
+
+
+
+</body>
+</html>
