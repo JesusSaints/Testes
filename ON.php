0c0f9268c66e6c7af5dbecf517c7d02475ee7f68
JesusSaints <92698720+JesusSaints@users.noreply.github.com>
2021-11-05 20:26:38 -0300

    On.php

---
+++ ON.php
@@ -0,0 +1,33 @@
+<?php
+		$paginas ["Jesus","Home","Contato"];
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Jesus Cristo</title>
+	<style type="text/css">
+		*{
+				margin: 30px;
+				padding: 0;
+				box-sizing: border-box;
+		}
+		header{
+			background:#800000;
+			padding: 20px 15px;
+			text-align: center;
+
+
+		} 
+
+
+
+
+	</style>
+</head>
+<body>
+
+</body>
+</html>
