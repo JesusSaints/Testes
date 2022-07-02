cef6681ea50556425f78273cb1378fef03abdf7b
JesusSaints <92698720+JesusSaints@users.noreply.github.com>
2022-07-02 17:30:13 -0300

    Oja.php

---
+++ Oja.php
@@ -0,0 +1,37 @@
+<?php
+		$paginas = ['Jesus','Senhor','Contato'];
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Jesus Crist</title>
+	<style type="text/css">
+		*{
+			margin: 20px 0px;
+			padding: 0px;
+			box-sizing: border-box;
+		}
+		header{
+			background: #191970;
+			padding: 0px;
+			text-align: center;
+
+
+
+
+
+
+		}
+
+
+
+	</style>
+</head>
+<body>
+
+</body>
+</html>
