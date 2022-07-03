052e073779569e9c9fa72a0f10b427322cea8211
JesusSaints <92698720+JesusSaints@users.noreply.github.com>
2022-07-02 23:28:26 -0300

    Jq.php

---
+++ Jq.php
@@ -0,0 +1,38 @@
+<?php
+		$paginas = ['Jesus','Senhor','Contato'];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Jesus Cristo</title>
+	<style type="text/css">
+		*{
+			margin: 0;
+			padding: 0;
+			box-sizing: border-box;
+
+
+
+		}
+		header{
+			background-color:#191970;
+			padding: 15px 20px;
+			text-align: center;
+
+
+
+
+		}
+
+
+
+
+
+	</style>
+</head>
+<body>
+
+</body>
+</html>
