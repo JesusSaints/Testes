a224ed0238818a5b836e9c8da68d6ce6ae8c5946
JesusSaints <92698720+JesusSaints@users.noreply.github.com>
2022-07-04 18:17:58 -0300

    Lk.php

---
+++ Lk.php
@@ -0,0 +1,45 @@
+<?php
+
+				$paginas = [Home,Sobre,contato];
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Jesus Cristo</title>
+	<style type="text/css">
+		*{
+			margin: 0;
+			padding: 0;
+			box-sizing: border-box;
+		}
+		header{
+			background: #191970;
+			padding: 8px 12px;
+			text-align: center;
+		}
+		a{
+			display: inline-block;
+			color: white;
+			margin: 15px;
+			font-size: 18px;
+		}
+</style>
+</head>
+	<header>
+			<?php
+
+					foreach ($paginas as $key => $value) {
+					echo '<a href="?page='.$key.'">'.$key.'</a>';
+					}
+			?>
+	</header>
+
+<body>
+
+
+</body>
+</html>
