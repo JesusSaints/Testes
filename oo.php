86095696a22799e32729f246b5ef8e18a9a5dacd
JesusSaints <92698720+JesusSaints@users.noreply.github.com>
2022-07-09 16:10:44 -0300

    oo.php

---
+++ oo.php
@@ -0,0 +1,88 @@
+	<?php
+		$paginas = ['Home'=>'','Sobre'=>'','Conato'=>''];
+		$pagevrt = ['Jesus'=>'','Santo'=>'Rei',''=>'Senhor','Perfeito'=>'','Deus'=>''];
+
+	?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Jesus Cristo</title>
+	<style type="text/css">
+		*{
+			margin: 0;
+			padding: 0;
+			box-sizing: border-box;
+		}
+	header{
+		background: #363636;
+		padding: 0px 7px;
+		text-align: center;
+		position: relative;
+		bottom: 4px;
+	}
+		a{
+			display: inline-block;
+			margin: 10px;
+			color: white;
+			font-size: 18px;
+		}
+		div.quadrado
+			{
+				//border: solid black;
+				//width: 1440px;
+				//height: 150px;
+			}
+			div#q1{
+				//background: #A52A2A;
+
+			}
+			img{
+			width: 100%;
+			height: 151px;
+	</style>
+	<style type="text/css">
+		*{
+			margin: 0;
+			padding: 0;
+			box-sizing: border-box;
+		}
+		header{
+			background: #363636;
+			padding: 100px 300px;
+			text-align: left;
+		}
+		a{
+			margin: 0 10px;
+			color: white;
+			font-size: 18px;
+		}
+
+
+	</style>
+		
+</head>
+<body>
+	<div class="quadrado" id="q1">
+		<img src="Imagens/Bn.png">
+		
+	</div>
+	<header>
+			<?php
+					foreach ($paginas as $key => $value) {
+					echo '<a href="?page='.$key.'">'.$key.'</a>';
+					}
+					foreach ($pagevrt as $key => $value) {
+					echo '<a href="?page='.$key.'">'.$key.'</a>';
+					}
+
+			?>
+
+	</header>
+
+
+
+
+</body>
+</html>
