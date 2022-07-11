a072ce419d3c90f4adc93cc48144f43051d94e61
JesusSaints <92698720+JesusSaints@users.noreply.github.com>
2022-07-10 22:28:38 -0300

    om.php

---
+++ om.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Jesus Cristo</title>
+	<style type="text/css">
+		*{
+			margin: 0;
+			padding: 0;
+			box-sizing: border-box;
+
+		}	
+		nav{
+			background: #808080;
+			margin: 31px 0px;
+			width: 150px;
+			height: 600px;
+
+		}
+
+
+
+		a{
+			display: none;
+			text-decoration: none;
+			margin: 0px 2px;
+			width: 150px;
+			height: 30px;
+
+
+			display: block;
+			color: white;
+			font-size: 18px;
+		}
+		a:hover{
+			background-color: #363636;
+			margin: 0;
+		}
+		.menu ul {
+			list-style: none;
+		}
+		div.retangulo{
+			width: 100%;
+			height: 150px;
+		}
+		nav#me{
+			position: relative;
+			bottom: 2px;
+
+		}
+
+
+		*{
+
+		}	
+		nav{
+			background: #363636;
+			margin: 31px 0px;
+			width: 150px;
+			height: 600px;
+
+		}
+
+
+
+		a{
+			display: inline-block;
+			text-decoration: none;
+			margin: 0px 2px;
+			width: 150px;
+			height: 30px;
+			color: white;
+			font-size: 18px;
+		}
+
+		a:hover{
+			background-color: #4F4F4F;
+			margin: 0;
+		}
+		.menu ul {
+			list-style: none;
+		}
+		nav.menu2{
+			width: 100%;
+			height: 30px;
+			text-align: center;
+		}
+		nav#me2{
+			position: relative;
+			bottom: 665px;
+		}
+	</style>
+</head>
+<body>
+	<div class="retangulo" id="r1">
+		<img src="Imagens/Bn.png"
+	</div>
+	<nav class="menu" id="me">
+		<ul>
+			<li><a href="?page="> Jesus </a></li>
+			<li><a href="?page="> Rei </a></li>
+			<li><a href="?page="> Sehor </a></li>
+			<li><a href="?page="> Perfeito </a></li>
+			<li><a href="?page="> Lindo </a></li>
+
+
+		</ul>	
+	</nav>
+		<nav class="menu2" id="me2">
+		<ul>
+			<a href="?page="> Jesus </a></li>
+			<a href="?page="> Rei </a></li>
+			<a href="?page="> Sehor </a></li>
+			<a href="?page="> Perfeito </a></li>
+			<a href="?page="> Lindo </a></li>
+
+
+		</ul>	
+	</nav>
+	</body>
+	</html>
